ff7f929244de2841ef5d569f1b265ae86d67ce7a
Chisel Wright <chisel@chizography.net>
2014-03-18 00:34:46 +0000

    Working copy of vaguely functional app

---
+++ src/net/chizography/droid/whogoesfirst/FingerChooserActivity.java
@@ -0,0 +1,12 @@
+package net.chizography.droid.whogoesfirst;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FingerChooserActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_finger_chooser);
+	}
+}
