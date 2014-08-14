33ac2430c927f8f8e8880777bef227f7afa9b98d
Chisel <chisel@chizography.net>
2014-08-14 17:00:24 +0300

    First attempt at the actual countdown

--- src/net/chizography/droid/whogoesfirst/CircleCountdown.java
+++ src/net/chizography/droid/whogoesfirst/CircleCountdown.java
@@ -2,12 +2,14 @@ package net.chizography.droid.whogoesfirst;
 import android.view.*;
 import android.widget.*;
 import android.content.*;
+import android.os.*;
 
 public class CircleCountdown {
 	
 	private View inflatedView;
+	private int countdownSeconds = 5;
 	
-	CircleCountdown(CirclesDrawingView cdv) {
+	CircleCountdown(final CirclesDrawingView cdv) {
 		LayoutInflater inflater = (LayoutInflater) cdv.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflatedView = inflater.inflate(R.layout.activity_finger_chooser, null, false);
 		TextView tv = (TextView) inflatedView.findViewById(R.id.txtTimer);
@@ -17,11 +19,23 @@ public class CircleCountdown {
 			return;
 		}
 		else {
-			cdv.setTimerText(3);
+			cdv.setTimerText(countdownSeconds);
 			cdv.setTimerTextVisible(true);
 			if (tv.getVisibility() != View.VISIBLE) {
 				cdv.setTimerTextVisible(true);
 			}
+			
+			new CountDownTimer(countdownSeconds * 1000, 1000) {
+
+				public void onTick(long millisUntilFinished) {
+					int i = (int) millisUntilFinished / 1000;
+					cdv.setTimerText(i);
+				}
+
+				public void onFinish() {
+					cdv.setTimerText(0);
+				}
+			}.start();
 		}
 	}
 }
