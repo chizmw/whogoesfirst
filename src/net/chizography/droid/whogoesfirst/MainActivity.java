9d6a934fd1e330bf1ff0bc364e3da614ee46cd5e
Chisel <chisel@chizography.net>
2014-08-15 17:10:29 +0300

    Remove titlebar & run fullscreen

--- src/net/chizography/droid/whogoesfirst/MainActivity.java
+++ src/net/chizography/droid/whogoesfirst/MainActivity.java
@@ -10,6 +10,12 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+							 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_finger_chooser);
 	}
 }
