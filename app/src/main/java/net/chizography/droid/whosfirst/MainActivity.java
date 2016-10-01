502889fce2ff81270b2038808c9e78aca1495ac1
Chisel <chisel@chizography.net>
2016-10-01 19:34:16 +0100

    Add Answers from fabric.io
    Previously removed because we thought we'd use the google/firebase analytics.
    The email and info from Fabric is too awesome to lose.

--- app/src/main/java/net/chizography/droid/whosfirst/MainActivity.java
+++ app/src/main/java/net/chizography/droid/whosfirst/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import com.crashlytics.android.answers.Answers;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        Fabric.with(this, new Answers());
 
 		// remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
