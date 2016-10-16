c385568b71193bc7d04ad7a7f45d8218203e386b
Chisel <chisel@chizography.net>
2016-10-16 14:08:43 +0100

    Improve 'final positions' picking logic
    1. understand hashsets better
    2. write logic to deal with cases raised by new understanding

--- app/src/main/java/net/chizography/droid/whosfirst/FingerCircles.java
+++ app/src/main/java/net/chizography/droid/whosfirst/FingerCircles.java
@@ -3,10 +3,13 @@ package net.chizography.droid.whosfirst;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class FingerCircles {
     
@@ -143,26 +146,89 @@ public class FingerCircles {
             paint
         );
     }
+
+    private void dumpCirclePointers() {
+        if (mCirclePointer == null) {
+            AppLog.d("CIRCLEDUMP| null");
+            return;
+        }
+        AppLog.d(
+            String.format(
+                Locale.getDefault(),
+                "CIRCLEDUMP| %d circle pointers",
+                mCirclePointer.size()
+            )
+        );
+
+        for (CircleArea circle : mCircles) {
+            if (circle == null) {
+                AppLog.w(
+                    "CIRCLEDUMP: NULL circle snuck in to the pot"
+                );
+                continue;
+            }
+            AppLog.d(
+                String.format(
+                    Locale.getDefault(),
+                    "CIRCLEDUMP| CirclePointer (%4d, %4d); pointers=%d",
+                    circle.getCenterX(),
+                    circle.getCenterY(),
+                    circle.getPointerCount()
+                )
+            );
+        }
+    }
     
     public void pickPlayerOrder(final boolean pickedWinner) {
-        if (pickedWinner)
+        if (pickedWinner) {
+            //AppLog.v("pickPlayerOrder(): pickedWinner is true; nothing to do");
             return;
+        }
+        AppLog.d("Time to pickPlayerOrder()");
+        dumpCirclePointers();
 
-        // I'm sure there is a more elegant way to do this
+        // check that we actually have circle pointers
+        if (mCirclePointer == null) {
+            AppLog.e("pickPlayerOrder(): mCirclePointer == null");
+            return;
+        }
+        // and extra check that we have non-zero circle pointers
+        if (mCirclePointer.size() < 1) {
+            AppLog.e("pickPlayerOrder(): size < 1");
+            return;
+        }
+
+        /*
+         * null circles can appear when we remove circles from the hashset or pointer list
+         * rather than re-arrange them all and complicate things even further, just ignore them at this stage.
+         */
+
+
+        /* I'm sure there is a more elegant way to do this;
+         * basically pick a random number, and keep going until we pick a non-null circle from the list of pointers
+         */
+        CircleArea ca = null;
         Random rand = new Random();
-        int ri = rand.nextInt((mCirclePointer.size()));
-        CircleArea ca = mCirclePointer.get(ri);
-        if(null==ca) {
-            AppLog.w("Picked a pointer with no circle");
-        }
-        if (ca != null) {
-            ca.setFirstPlayer(true);
-        }
-        if (ca != null) {
-            ca.setStartPosition(1);
+
+        while (ca == null) {
+            int ri = rand.nextInt((mCirclePointer.size()));
+            ca = mCirclePointer.get(ri);
+
+            AppLog.d("Winning position " + ri);
+            if(null==ca) {
+                AppLog.w("pickPlayerOrder(): Picked a pointer with no circle");
+            }
+            if (ca != null) {
+                ca.setFirstPlayer(true);
+                ca.setStartPosition(1);
+            }
         }
 
         // assign remaining startPosition values
+        /* we start with the number of remaining positions set to the total number of pointers;
+         * keep picking circles and when we find one that's not null and doesn't (yet) have a start position,
+         * set the start position for that circle, and decrement the number of remaining positions to assign
+         */
         int remainingPositions = mCirclePointer.size();
         // we have already assigned #1
         while (remainingPositions > 1) {
@@ -170,15 +236,26 @@ public class FingerCircles {
             // find a circle with no assigned start position
             do {
                 int randomIndex = rand.nextInt(mCirclePointer.size());
+                AppLog.d("randomIndex=" + randomIndex);
                 position_circle = mCirclePointer.get(randomIndex);
+                if (position_circle == null) {
+                    /* this can happen when "circles merge" ... it's a feature of HashSets */
+                    AppLog.d("pickPlayerOrder(): remainingPositions: dammit! a null circle!");
+                    /* we don't need to assign a start position for this bugger; decrement so we
+                     * know we've dealt with this non-circle
+                     */
+                    remainingPositions--;
+                }
             }
-            while (position_circle.hasStartPosition());
+            while (position_circle == null || position_circle.hasStartPosition());
 
             // assign a position
             position_circle.setStartPosition(remainingPositions);
             // reduce the number of available positions
             remainingPositions--;
         }
+
+        // when we fall out here we've assigned all start positions
 	}
     
     public CircleArea scanForTouchedCircle(final MotionEvent event){
