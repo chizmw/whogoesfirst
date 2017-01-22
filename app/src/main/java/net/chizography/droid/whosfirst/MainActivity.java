1d25bd98c82d743260aec32f4ab4f2332aa704b3
Chisel <chisel@chizography.net>
2017-01-22 20:06:54 +0000

    Add event: "Opened App"

--- app/src/main/java/net/chizography/droid/whosfirst/MainActivity.java
+++ app/src/main/java/net/chizography/droid/whosfirst/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.WindowManager;
 import com.crashlytics.android.answers.Answers;
 import io.fabric.sdk.android.Fabric;
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.CustomEvent;
 
 public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -22,7 +23,9 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 							 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
+
+		Answers.getInstance().logCustom(new CustomEvent("Opened App"));
+
 		final Intent intent = new Intent(this, FingerCircleActivity.class);
 		startActivity(intent);
 		this.finish();
