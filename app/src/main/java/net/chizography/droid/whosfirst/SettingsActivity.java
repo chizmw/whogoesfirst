11d21b8c19a5df0f4418433775f82129144fb4a1
Chisel <chisel@chizography.net>
2017-01-22 16:10:29 +0000

    Add event: "Opened Settings"

--- app/src/main/java/net/chizography/droid/whosfirst/SettingsActivity.java
+++ app/src/main/java/net/chizography/droid/whosfirst/SettingsActivity.java
@@ -3,10 +3,16 @@ package net.chizography.droid.whosfirst;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.CustomEvent;
+
 public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Answers.getInstance().logCustom(new CustomEvent("Opened Settings"));
+
 
         // Display the preferences fragment as the content of the activity
         getFragmentManager().beginTransaction()
